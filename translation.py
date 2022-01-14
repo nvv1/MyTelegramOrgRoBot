8690d8e592cc42a85f48296f33af53d430cdf97c
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-01-14 21:54:59 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,10 +1,11 @@
 class Translation(object):
     START_TEXT = """Hola🤞!
 I am Telegram Api-id and Api-Hash Scrapper. 
-IF you Dont believe The dev of this bot dont use this bot and use [my.telegram.org](my.telegram.org) 
+IF you Dont believe The dev of this bot dont use this bot and use my.telegram.org 
 Now Send Your phone Number to get api details. 
-If Bot stops when the way of halfly getting api details please ensureto type /start. 
-Thanks For using meh. 
+If Bot stops when the way of halfly getting api details please ensure to type /start. 
+I am Also Working Along With @TeleRoidSessionBot For Generating Pyrogram And TeleThon Session String. 
+Thanks For using this Bot. 
 
 """
     AFTER_RECVD_CODE_TEXT = """okay,
@@ -12,7 +13,7 @@ Thanks For using meh.
 
 """
     BEFORE_SUCC_LOGIN = "™️ Recieved Code Successfully . Scarpping From web page ..."
-    ERRED_PAGE = "something went wrong. failed to get app id. \n\n@TeleRoid14"
-    CANCELLED_MESG = "ok Then! Please /start the bot to restart"
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+    ERRED_PAGE = "something went wrong. Failed to get app id. \n\n@TeleRoid14"
+    CANCELLED_MESG = "Ok Then! Please /start the bot to restart"
+    IN_VALID_CODE_PVDED = "Sorry, but the input does not seem to be a valid Telegram Web-Login code"
+    IN_VALID_PHNO_PVDED = "Sorry, but the input does not seem to be a valid phone number"
